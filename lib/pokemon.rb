49270cc3fd73266d1b771e611e708336b3a5b4cb
Josh Raiborde <joshsrai@yahoo.com>
2020-09-01 19:29:31 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,14 @@
 class Pokemon
+  attr_accessor :id, :name, :type, :hp, :db
+  
+  def initialize(id:, name:, type:, hp: nil, db:)
+    @id = id
+    @name = name 
+    @type = type 
+    @hp = hp 
+    @db = db 
+  end
+  
+  
+  
 end
